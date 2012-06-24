4bdb33d8f650694a1d19b6f5e48ec1c662b6e0ae
Reamer <philipp.dallig@web.de>
2012-06-24 19:06:24 +0200

    kara rewrite

---
+++ sql/custom/ready for DB/mangos_karazhan.sql
@@ -0,0 +1,6 @@
+-- Attumen
+UPDATE creature_template SET Scriptname = 'boss_midnight' WHERE entry = 16152;
+
+-- Terestian Illhoof
+UPDATE creature_template SET AIName = 'NullAI', Scriptname = '' WHERE entry = 17248;
+UPDATE creature_template SET AIName = 'NullAI', Scriptname = '' WHERE entry = 17265;
