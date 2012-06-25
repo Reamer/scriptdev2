429dad9d321218490a0027023a99fc1a928370d0
Reamer <philipp.dallig@web.de>
2012-06-25 17:23:43 +0200

    kara sql

--- sql/custom/ready for DB/mangos_karazhan.sql
+++ sql/custom/ready for DB/mangos_karazhan.sql
@@ -1,6 +1,9 @@
 -- Attumen
 UPDATE creature_template SET Scriptname = 'boss_midnight' WHERE entry = 16152;
 
+-- Moroes Guest
+UPDATE creature_template Set MovementType = 0 WHERE entry IN (17007, 19872, 19873, 19874, 19875, 19876);
+
 -- Terestian Illhoof
 UPDATE creature_template SET AIName = 'NullAI', Scriptname = '' WHERE entry = 17248;
 UPDATE creature_template SET AIName = 'NullAI', Scriptname = '' WHERE entry = 17265;
