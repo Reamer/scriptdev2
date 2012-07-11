12d75094360db4b2825e61e79fda3b9f4732bb96
Reamer <philipp.dallig@web.de>
2012-07-11 15:45:19 +0200

    gnomeregan from sd2

--- scripts/eastern_kingdoms/gnomeregan/boss_thermaplugg.cpp
+++ scripts/eastern_kingdoms/gnomeregan/boss_thermaplugg.cpp
@@ -59,8 +59,8 @@ struct MANGOS_DLL_DECL boss_thermapluggAI : public ScriptedAI
     sBombFace* m_asBombFaces;
     float m_afSpawnPos[3];
 
-    GUIDList m_lSummonedBombGUIDs;
-    GUIDList m_lLandedBombGUIDs;
+    GuidList m_lSummonedBombGUIDs;
+    GuidList m_lLandedBombGUIDs;
 
     void Reset()
     {
@@ -114,13 +114,11 @@ struct MANGOS_DLL_DECL boss_thermapluggAI : public ScriptedAI
 
     void JustReachedHome()
     {
-        if (!m_pInstance || m_pInstance->GetData(TYPE_THERMAPLUGG) == FAIL)
-            return;
-
-        m_pInstance->SetData(TYPE_THERMAPLUGG,FAIL);
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_THERMAPLUGG, FAIL);
 
         // Remove remaining bombs
-        for (GUIDList::const_iterator itr = m_lSummonedBombGUIDs.begin(); itr != m_lSummonedBombGUIDs.end(); ++itr)
+        for (GuidList::const_iterator itr = m_lSummonedBombGUIDs.begin(); itr != m_lSummonedBombGUIDs.end(); ++itr)
         {
             if (Creature* pBomb = m_creature->GetMap()->GetCreature(*itr))
                 pBomb->ForcedDespawn();
@@ -160,7 +158,7 @@ struct MANGOS_DLL_DECL boss_thermapluggAI : public ScriptedAI
         // Movement of Summoned mobs
         if (!m_lLandedBombGUIDs.empty())
         {
-            for (GUIDList::const_iterator itr = m_lLandedBombGUIDs.begin(); itr != m_lLandedBombGUIDs.end(); ++itr)
+            for (GuidList::const_iterator itr = m_lLandedBombGUIDs.begin(); itr != m_lLandedBombGUIDs.end(); ++itr)
             {
                 if (Creature* pBomb = m_creature->GetMap()->GetCreature(*itr))
                     pBomb->GetMotionMaster()->MoveFollow(m_creature, 0.0f, 0.0f);
