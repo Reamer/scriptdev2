e3de048421979deb7b2cc8e899a33271d7e377d0
Reamer <philipp.dallig@web.de>
2012-07-11 15:47:52 +0200

    landscapes in eastern_kindoms from sd2

--- scripts/eastern_kingdoms/stranglethorn_vale.cpp
+++ scripts/eastern_kingdoms/stranglethorn_vale.cpp
@@ -17,196 +17,96 @@
 /* ScriptData
 SDName: Stranglethorn_Vale
 SD%Complete: 100
-SDComment: Quest support: 592, 8193
+SDComment: Quest support: 592
 SDCategory: Stranglethorn Vale
 EndScriptData */
 
 /* ContentData
 mob_yenniku
-npc_riggle_bassbait
 EndContentData */
 
 #include "precompiled.h"
-#include "GameEventMgr.h"
-
-enum
-{
-    SAY_START               = -1510356,
-    SAY_WINNER              = -1510357,
-    SAY_END                 = -1510358,
-    QUEST_MASTER_ANGLER     = 8193,
-};
 
 /*######
 ## mob_yenniku
 ######*/
 
+enum
+{
+    SPELL_YENNIKUS_RELEASE      = 3607,
+
+    QUEST_ID_SAVING_YENNIKU     = 592,
+
+    FACTION_ID_HORDE_GENERIC    = 83,                       // Note: faction may not be correct!
+};
+
 struct MANGOS_DLL_DECL mob_yennikuAI : public ScriptedAI
 {
-    mob_yennikuAI(Creature *c) : ScriptedAI(c)
-    {
-        bReset = false;
-        Reset();
-    }
+    mob_yennikuAI(Creature* pCreature) : ScriptedAI(pCreature) { Reset(); }
 
-    uint32 Reset_Timer;
-    bool bReset;
+    uint32 m_uiResetTimer;
 
-    void Reset()
-    {
-        Reset_Timer = 0;
-        m_creature->HandleEmote(EMOTE_STATE_NONE);
-    }
+    void Reset() { m_uiResetTimer = 0; }
 
-    void SpellHit(Unit *caster, const SpellEntry *spell)
+    void SpellHit(Unit* pCaster, const SpellEntry* pSpell)
     {
-        if (caster->GetTypeId() == TYPEID_PLAYER)
+        if (pSpell->Id == SPELL_YENNIKUS_RELEASE && pCaster->GetTypeId() == TYPEID_PLAYER)
         {
-                                                            //Yenniku's Release
-            if (!bReset && ((Player*)caster)->GetQuestStatus(592) == QUEST_STATUS_INCOMPLETE && spell->Id == 3607)
+            if (!m_uiResetTimer && ((Player*)pCaster)->GetQuestStatus(QUEST_ID_SAVING_YENNIKU) == QUEST_STATUS_INCOMPLETE)
             {
-                m_creature->HandleEmote(EMOTE_STATE_STUN);
-                m_creature->CombatStop();                   //stop combat
-                m_creature->DeleteThreatList();             //unsure of this
-                m_creature->setFaction(83);                 //horde generic
-
-                bReset = true;
-                Reset_Timer = 60000;
+                m_uiResetTimer = 60000;
+                EnterEvadeMode();
             }
         }
-        return;
     }
 
-    void Aggro(Unit *who) {}
-
-    void UpdateAI(const uint32 diff)
+    void EnterEvadeMode()
     {
-        if (bReset)
-            if (Reset_Timer < diff)
+        if (m_uiResetTimer)
         {
-            EnterEvadeMode();
-            bReset = false;
-            m_creature->setFaction(28);                     //troll, bloodscalp
-        }
-        else Reset_Timer -= diff;
+            m_creature->RemoveAllAuras();
+            m_creature->DeleteThreatList();
+            m_creature->CombatStop(true);
+            m_creature->LoadCreatureAddon(true);
 
-        //Return since we have no target
+            m_creature->SetLootRecipient(NULL);
+
+            m_creature->HandleEmote(EMOTE_STATE_STUN);
+            m_creature->SetFactionTemporary(FACTION_ID_HORDE_GENERIC, TEMPFACTION_RESTORE_REACH_HOME);
+        }
+        else
+            ScriptedAI::EnterEvadeMode();
+    }
+
+    void UpdateAI(const uint32 uiDiff)
+    {
+        if (m_uiResetTimer)
+        {
+            if (m_uiResetTimer <= uiDiff)
+            {
+                m_creature->HandleEmote(EMOTE_STATE_NONE);
+                m_uiResetTimer = 0;
+                EnterEvadeMode();
+            }
+            else
+                m_uiResetTimer -= uiDiff;
+        }
+
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
         DoMeleeAttackIfReady();
     }
 };
+
 CreatureAI* GetAI_mob_yenniku(Creature *_Creature)
 {
     return new mob_yennikuAI (_Creature);
 }
 
 /*######
-##npc_riggle_bassbait
+##
 ######*/
-/**
- *  AI for Riggle Bassbait.
- *  This is the AI for Riggle Bassbait, see http://www.wowhead.com/?npc=15077
- *  @see ScriptedAI
- *  @author burned, gotisch
- */
-struct MANGOS_DLL_DECL npc_riggle_bassbaitAI : public ScriptedAI
-{
-    /**
-     *  Constructor of the Creature.
-     *  This is called when the creature is spawned.
-     *  @param c The Creature that this AI is for
-     */
-    npc_riggle_bassbaitAI(Creature *c) : ScriptedAI(c)
-    {
-        // This will keep the NPC active even if there are no players around!
-        c->SetActiveObjectState(true);
-        bEventAnnounced = bEventIsOver = bEventWinnerFound = false;
-        Reset();
-    }
-    /**
-     *  Flag to check if event was announced. True if event was announced.
-     */
-    bool bEventAnnounced;
-    /**
-     *  Flag to check if event is over. True if event is over.
-     */
-    bool bEventIsOver;
-    /**
-     *  Flag to check if someone won the event. True if someone has won.
-     */
-    bool bEventWinnerFound;
-
-    void Reset() { }
-
-    void Aggro(Unit *who) {}
-
-    void UpdateAI(const uint32 diff)
-    {
-        // Announce the event max 1 minute after being spawned. But only if Fishing extravaganza is running.
-        if (!bEventAnnounced && time(NULL) % 60 == 0 && IsHolidayActive(HOLIDAY_FISHING_EXTRAVAGANZA))
-        {
-            debug_log("SD2: npc_riggle_bassbait announce HOLIDAY_FISHING_EXTRAVAGANZA contest");
-            DoScriptText(SAY_START, m_creature);
-            m_creature->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER); //Quest&Gossip are now active
-            bEventAnnounced = true;
-        }
-        // The Event was started (announced) & It was not yet ended & One minute passed & the Fish are gone
-        if ( bEventAnnounced && !bEventIsOver && time(NULL) % 60 == 0 && !IsHolidayActive(HOLIDAY_FISHING_EXTRAVAGANZA))
-        {
-            debug_log("SD2: npc_riggle_bassbait end HOLIDAY_FISHING_EXTRAVAGANZA contest");
-            DoScriptText(SAY_END, m_creature);
-            bEventIsOver = true;
-        }
-        if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
-            return;
-
-        DoMeleeAttackIfReady();
-    }
-};
-/**
- * GossipHello for NPC Riggle Bassbait. 
- * This is called each time a Player tries to talk with the NPC.
- */
-bool GossipHello_npc_riggle_bassbait(Player* pPlayer, Creature* pCreature)
-{
-    if (pCreature->isQuestGiver()) // If the quest is still running.
-    {
-        pPlayer->PrepareQuestMenu(pCreature->GetObjectGuid());
-        pPlayer->SEND_GOSSIP_MENU(7614, pCreature->GetObjectGuid());
-        return true;
-    }
-    // The Quest is not there anymore 
-    // There is a winner!
-    pPlayer->SEND_GOSSIP_MENU(7714, pCreature->GetObjectGuid());
-    return true;
-}
-
-bool ChooseReward_npc_riggle_bassbait(Player* pPlayer, Creature* pCreature, const Quest* pQuest)
-{
-    // TODO: check if this can only be called if NPC has QUESTGIVER flag.
-    if (pQuest->GetQuestId() == QUEST_MASTER_ANGLER && ((npc_riggle_bassbaitAI*)(pCreature->AI()))->bEventWinnerFound == false)
-    {
-        DoScriptText(SAY_WINNER, pCreature,pPlayer);
-        pCreature->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER);
-        ((npc_riggle_bassbaitAI*)(pCreature->AI()))->bEventWinnerFound = true;
-        Creature* creature2 = GetClosestCreatureWithEntry(pCreature,15087,60.0f);
-        if (creature2)
-        {
-            creature2->SetFlag(UNIT_NPC_FLAGS,UNIT_NPC_FLAG_QUESTGIVER);
-        } else {
-            debug_log("Could not change flag of Jang");
-        }
-        return true;
-    }
-    return true;
-}
-
-CreatureAI* GetAI_npc_riggle_bassbait(Creature* pCreature)
-{
-    return new npc_riggle_bassbaitAI(pCreature);
-}
 
 void AddSC_stranglethorn_vale()
 {
@@ -215,12 +115,5 @@ void AddSC_stranglethorn_vale()
     pNewScript = new Script;
     pNewScript->Name = "mob_yenniku";
     pNewScript->GetAI = &GetAI_mob_yenniku;
-    pNewScript->RegisterSelf();
-
-    pNewScript = new Script;
-    pNewScript->Name = "npc_riggle_bassbait";
-    pNewScript->GetAI = &GetAI_npc_riggle_bassbait;
-    pNewScript->pGossipHello = &GossipHello_npc_riggle_bassbait;
-    pNewScript->pQuestRewardedNPC = &ChooseReward_npc_riggle_bassbait;
     pNewScript->RegisterSelf();
 }
