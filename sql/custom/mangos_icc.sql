a523af33c6748d80df696eb5aed0257317ea83ce
Reamer <philipp.dallig@web.de>
2012-08-08 22:47:55 +0200

    erster boss ICC

---
+++ sql/custom/mangos_icc.sql
@@ -0,0 +1,6 @@
+-- ---------
+-- Marrowgar
+-- ---------
+
+UPDATE `creature_template` SET `ScriptName`='boss_lord_marrowgar', `AIName`='' WHERE `entry`= 36612;
+UPDATE `creature_template` SET `ScriptName`='npc_bone_spike', `AIName`='' WHERE `entry`= 36619;
