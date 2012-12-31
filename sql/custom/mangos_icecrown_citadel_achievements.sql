7d00b0a0efe85a98e402fb791743cf0a693a92ea
Reamer <philipp.dallig@web.de>
2012-12-31 17:16:22 +0100

    ICC achievements

---
+++ sql/custom/mangos_icecrown_citadel_achievements.sql
@@ -0,0 +1,188 @@
+-- Lord Marrowgar
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 13393, 12775, 13394, 12962);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12775, 18, 0, 0),
+(12775, 12, 0, 0),
+-- 25 nh
+(12962, 18, 0, 0),
+(12962, 12, 1, 0),
+-- 10 h
+(13393, 18, 0, 0),
+(13393, 12, 2, 0),
+-- 25 h
+(13394, 18, 0, 0),
+(13394, 12, 3, 0);
+
+-- Lady Deathwhisper
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12776, 12997, 12995, 12998);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12776, 18, 0, 0),
+(12776, 12, 0, 0),
+-- 25 nh
+(12997, 18, 0, 0),
+(12997, 12, 1, 0),
+-- 10 h
+(12995, 18, 0, 0),
+(12995, 12, 2, 0),
+-- 25 h
+(12998, 18, 0, 0),
+(12998, 12, 3, 0);
+
+-- Gunship Battle
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12777, 13080, 13079, 13081);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12777, 18, 0, 0),
+(12777, 12, 0, 0),
+-- 25 nh
+(13080, 18, 0, 0),
+(13080, 12, 1, 0),
+-- 10 h
+(13079, 18, 0, 0),
+(13079, 12, 2, 0),
+-- 25 h
+(13081, 18, 0, 0),
+(13081, 12, 3, 0);
+
+-- Deathbringer Saurfang
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12778, 13036, 13035, 13037);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12778, 18, 0, 0),
+(12778, 12, 0, 0),
+-- 25 nh
+(13036, 18, 0, 0),
+(13036, 12, 1, 0),
+-- 10 h
+(13035, 18, 0, 0),
+(13035, 12, 2, 0),
+-- 25 h
+(13037, 18, 0, 0),
+(13037, 12, 3, 0);
+
+-- Festergut
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12977, 12982, 12986, 12967);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12977, 18, 0, 0),
+(12977, 12, 0, 0),
+-- 25 nh
+(12982, 18, 0, 0),
+(12982, 12, 1, 0),
+-- 10 h
+(12986, 18, 0, 0),
+(12986, 12, 2, 0),
+-- 25 h
+(12967, 18, 0, 0),
+(12967, 12, 3, 0);
+
+-- Rotface
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12984, 12966, 12985, 12983);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12984, 18, 0, 0),
+(12984, 12, 0, 0),
+-- 25 nh
+(12966, 18, 0, 0),
+(12966, 12, 1, 0),
+-- 10 h
+(12985, 18, 0, 0),
+(12985, 12, 2, 0),
+-- 25 h
+(12983, 18, 0, 0),
+(12983, 12, 3, 0);
+
+-- Professor Putricide
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12987, 12968, 12988, 12981);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12987, 18, 0, 0),
+(12987, 12, 0, 0),
+-- 25 nh
+(12968, 18, 0, 0),
+(12968, 12, 1, 0),
+-- 10 h
+(12988, 18, 0, 0),
+(12988, 12, 2, 0),
+-- 25 h
+(12981, 18, 0, 0),
+(12981, 12, 3, 0);
+
+-- Blood Prince Council
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 13033, 12969, 13034, 13032);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(13033, 18, 0, 0),
+(13033, 12, 0, 0),
+-- 25 nh
+(12969, 18, 0, 0),
+(12969, 12, 1, 0),
+-- 10 h
+(13034, 18, 0, 0),
+(13034, 12, 2, 0),
+-- 25 h
+(13032, 18, 0, 0),
+(13032, 12, 3, 0);
+
+-- Blood-Queen Lana'thel
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12780, 13012, 13011, 13013);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12780, 18, 0, 0),
+-- 25 nh
+(13012, 18, 0, 0),
+-- 10 h
+(13011, 18, 0, 0),
+-- 25 h
+(13013, 18, 0, 0);
+
+-- Valithria Dreamwalker
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12978, 12971, 12979, 12980);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12978, 18, 0, 0),
+(12978, 12, 0, 0),
+-- 25 nh
+(12971, 18, 0, 0),
+(12971, 12, 1, 0),
+-- 10 h
+(12979, 18, 0, 0),
+(12979, 12, 2, 0),
+-- 25 h
+(12980, 18, 0, 0),
+(12980, 12, 3, 0);
+
+-- Sindragosa
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 12822, 12972, 12996, 12989);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(12822, 18, 0, 0),
+(12822, 12, 0, 0),
+-- 25 nh
+(12972, 18, 0, 0),
+(12972, 12, 1, 0),
+-- 10 h
+(12996, 18, 0, 0),
+(12996, 12, 2, 0),
+-- 25 h
+(12989, 18, 0, 0),
+(12989, 12, 3, 0);
+
+-- Lich King
+DELETE FROM achievement_criteria_requirement WHERE criteria_id IN ( 13246, 13244, 13247, 13245);
+INSERT INTO achievement_criteria_requirement VALUES
+-- 10 nh
+(13246, 18, 0, 0),
+(13246, 12, 0, 0),
+-- 25 nh
+(13244, 18, 0, 0),
+(13244, 12, 1, 0),
+-- 10 h
+(13247, 18, 0, 0),
+(13247, 12, 2, 0),
+-- 25 h
+(13245, 18, 0, 0),
+(13245, 12, 3, 0);
+
